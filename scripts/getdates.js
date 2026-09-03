cf1149d48573f4a37b70c4cc4d9bf11c5c79e19c
ERICKOTI20051 <otienoerick20051@gmail.com>
2026-09-03 06:39:50 +0300

    Updated homepage with CSS

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Display the current year in the footer
+document.getElementById("currentyear").textContent = currentYear;
+
+// Display the date the document was last modified
+document.getElementById("lastModified").innerHTML =
+    "Last Modification: " + document.lastModified;
